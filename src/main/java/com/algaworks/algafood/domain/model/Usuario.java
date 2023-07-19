375c6a0e65fe77beeded28860cf936358a443a30
Matheus Vidal <matheusvidal97@gmail.com>
2023-07-18 21:37:38 -0300

    add groups permissions in user

--- src/main/java/com/algaworks/algafood/domain/model/Usuario.java
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -5,6 +5,8 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,15 +19,24 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String nome;
+
     @Column(nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String senha;
 
     @CreationTimestamp
     @Column(nullable = false)
     private LocalDateTime dataCriacao;
+
+    @ManyToMany
+    @JoinTable(name = "usuario_grupo_permissoes",
+                joinColumns = @JoinColumn(name = "usuario_id"),
+                inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private List<Grupo> grupos = new ArrayList<>();
 
 }

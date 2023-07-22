bfc3851344621da4ecc03ac6fb431f3a173fdf10
Matheus Vidal <matheusvidal97@gmail.com>
2023-07-22 00:56:29 -0300

    fix table restaurante and add test data

--- src/main/resources/db/migration/V004__cria-varias-tabelas.sql
+++ src/main/resources/db/migration/V004__cria-varias-tabelas.sql
@@ -45,7 +45,7 @@ create table restaurante (
                              data_atualizacao timestamp not null,
                              data_cadastro timestamp not null,
 
-                             endereco_cidade_id BIGSERIAL,
+                             endereco_cidade_id bigint,
                              endereco_cep varchar(9),
                              endereco_logradouro varchar(100),
                              endereco_numero varchar(20),

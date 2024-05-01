8ec3559ee079f61bbdf9ae409ee10c668553af3c
Vidal <jmvidal@Stefanini.com>
2024-05-01 11:05:30 -0300

    teste retorno 201 quando cadastrar nova cosinha

--- src/test/java/com/algaworks/algafood/domain/service/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/domain/service/CadastroCozinhaIT.java
@@ -3,14 +3,18 @@ package com.algaworks.algafood.domain.service;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import javax.validation.ConstraintViolationException;
 
@@ -19,18 +23,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 //@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 
     @LocalServerPort
     private int port;
     @Autowired
     private CadastroCozinhaService cadastroCozinhaService;
+    //@Autowired
+    //private Flyway flyway;
+    //@Autowired
+    //private DatabaseCleaner databaseCleaner;
 
     @BeforeEach
     void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        //flyway.migrate();
+        //databaseCleaner.clearTables();
     }
 
     @Test
@@ -99,6 +111,19 @@ class CadastroCozinhaIT {
                 .then()
                 .body("", Matchers.hasSize(4))
                 .body("nome", Matchers.hasItems("Indiana", "Taillandesa"));
+    }
+
+    @Test
+    void deveRetornarStatus201_QuandoCadastrarCozinha() {
+
+        RestAssured.given()
+                .body("{ \"nome\": \"Chinesa\" }")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
     }
 
 

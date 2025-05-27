64724deffb4f16f064bf5c92f3b642fb0f72260f
Vidal <matheusvidal97@gmail.com>
2025-05-27 10:43:45 -0300

    Ajuste no modelMapper para versão 3.0.0

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.core.modelmapper;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +11,15 @@ public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
+		var modelMapper = new ModelMapper();
+
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+				Endereco.class, EnderecoModel.class);
+
+		enderecoToEnderecoModelTypeMap.<String>addMapping(
+				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+
 		return new ModelMapper();
 	}
-	
 }
